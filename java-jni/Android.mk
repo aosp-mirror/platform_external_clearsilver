5158410ad2e8590d980147a19bf836e42332700a
David 'Digit' Turner <digit@google.com>
2010-12-10 22:30:48 +0100

    Better support for 64-bit clearsilver build.
    This patch ensures that ClearSilver is built as 64-bit even if a 32-bit
    host toolchain is used by default (e.g. when building the Linux SDK).
    
    Change-Id: I349043817fd1fe2f1afa7914469ffa5277ed4c3b

--- java-jni/Android.mk
+++ java-jni/Android.mk
@@ -36,6 +36,10 @@ LOCAL_CFLAGS += -fPIC
 # This forces a 64-bit build for Java6
 LOCAL_CFLAGS += -m64
 LOCAL_LDFLAGS += -m64
+# We use the host compilers because the Linux SDK build
+# uses a 32-bit toolchain that can't handle -m64
+LOCAL_CC := $(CC)
+LOCAL_CXX := $(CXX)
 
 LOCAL_NO_DEFAULT_COMPILER_FLAGS := true
 
